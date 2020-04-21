cbd4f70c077e695522f45f7f288293d2c0c3351b
aikrom <undercode.dev@gmail.com>
2020-04-21 16:57:55 +0700

    Separate db and server files

--- server.js
+++ server.js
@@ -1,31 +1,11 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const { dbConnect } = require('./db');
 
 const app = express();
 
-const {
-  SERVER_PORT,
-  MONGO_USERNAME,
-  MONGO_PASSWORD,
-  MONGO_HOSTNAME,
-  MONGO_PORT,
-  MONGO_DB
-} = process.env;
+const { SERVER_PORT } = process.env;
 
-const options = {
-  useNewUrlParser: true,
-  reconnectTries: Number.MAX_VALUE,
-  reconnectInterval: 500,
-  connectTimeoutMS: 10000,
-};
-
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
-mongoose.connect(url, options).then(function() {
-  console.log('MongoDB is connected');
-}).catch(function(err) {
-  console.log(err)
-});
+dbConnect();
 
 app.get('/', function(req, res) {
   res.send('Hello, world!');
